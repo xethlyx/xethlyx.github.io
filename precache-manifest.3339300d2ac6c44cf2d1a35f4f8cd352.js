41298351a1b542b9a81bd1d6b43b6ba4056c901c
xethlyx <46338199+xethlyx@users.noreply.github.com>
2019-08-04 18:26:36 -0400

    Updates

---
+++ precache-manifest.3339300d2ac6c44cf2d1a35f4f8cd352.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8e8a25dd42fb5fa49d4b5b2f560358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93a33f30c3a7b1f1ab14",
+    "url": "/static/css/main.ca831514.chunk.css"
+  },
+  {
+    "revision": "9106383d4d3cbd8a53ab",
+    "url": "/static/js/2.67841258.chunk.js"
+  },
+  {
+    "revision": "93a33f30c3a7b1f1ab14",
+    "url": "/static/js/main.df8ce6c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a1579a3221b90c3e2d01ee61c893b727",
+    "url": "/static/media/frozen_trees.a1579a32.jpg"
+  },
+  {
+    "revision": "8c861a22e239db8aa92e5d73c2758d7a",
+    "url": "/static/media/xe_logo.8c861a22.svg"
+  }
+]);
