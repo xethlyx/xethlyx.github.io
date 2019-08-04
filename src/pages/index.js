96975f2726c4ae21631209b2034e6cd2355be115
xethlyx <46338199+xethlyx@users.noreply.github.com>
2019-08-04 16:29:55 -0400

    Minor changes

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'React'
+
+class Index extends Component {
+  state = {
+    
+  }
+  render() {
+    return (
+      <div>
+        <p>index hi</p>
+      </div>
+    )
+  }
+}
+
+export default Index
