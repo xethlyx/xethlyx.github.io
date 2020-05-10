b61b450eee8a72af0472912de822e5946d214ae2
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-05-09 20:51:41 -0400

    Move viewport into app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../style.css';
 import '../mobile.css';
 
+import Head from 'next/head';
+
 function App({ Component, pageProps }: any) {
-	return <Component {...pageProps} />
+	return <>
+		<Head>
+			<title>Some Title</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+		<Component {...pageProps} />
+	</>
 }
 
 export default App;
